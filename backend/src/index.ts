d28f561b25d5acdafe6dfb5fc2bdadcc48c8ee30
unknown <emad.m.alnagar@gmail.com>
2024-06-02 01:35:57 +0200

    Create nodejs server

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("uploads/images"));
+
+mongoose.connect("mongodb://127.0.0.1:27017/meancrud");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port);
